9b0ed393dd66adcedc9a5aad0f96cab50b0bb6b7
Rory MacLeod <mail@rorymacleod.com>
2010-09-07 23:30:41 +0100

    New solution.

---
+++ Source/MSTestExtensions/ExtendedTestClassAttribute.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MSTestExtensions
+{
+    [Serializable]
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false)]
+    public class ExtendedTestClassAttribute : TestClassExtensionAttribute
+    {
+        public override Uri ExtensionId
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public override TestExtensionExecution GetExecution()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
